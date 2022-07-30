438b07f333aeff3898ab1239122da74c8d981fcf
thamph <poomz180@gmail.com>
2022-07-30 11:00:48 +0700

    custom scollbar style

--- src/popup/components/DefinitionsContainer.tsx
+++ src/popup/components/DefinitionsContainer.tsx
@@ -3,7 +3,7 @@ import DefinitionCard from "./DefinitionCard";
 
 const DefinitionsContainer = () => {
   return (
-    <div className="flex flex-col p-2 gap-2 w-[400px] h-[400px] rounded-lg bg-gray-100 overflow-scroll">
+    <div className="flex flex-col p-2 gap-2 w-[400px] h-[400px] rounded-lg bg-gray-100 overflow-y-scroll">
       <DefinitionCard />
       <DefinitionCard />
     </div>
