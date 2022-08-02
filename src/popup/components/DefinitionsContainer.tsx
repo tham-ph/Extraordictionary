3fe8374ca1a69f591c4fec5ed97ab35aa9ad3c71
thamph <poomz180@gmail.com>
2022-08-02 17:52:31 +0700

    show "Your search terms did not match any entries" when search not found

--- src/popup/components/DefinitionsContainer.tsx
+++ src/popup/components/DefinitionsContainer.tsx
@@ -17,7 +17,13 @@ const DefinitionsContainer = () => {
 
   return (
     <div className="flex flex-col p-2 gap-2 w-[400px] h-[400px] rounded-lg bg-gray-100 overflow-y-scroll">
-      {definitionCardList}
+      {definitionCardList.length === 0 ? (
+        <div className="flex justify-center items-center min-h-full">
+          <p className="text-2xl font-bold text-gray-400 text-center">Your search terms did not match any entries.</p>
+        </div>
+      ) : (
+        definitionCardList
+      )}
     </div>
   );
 };
