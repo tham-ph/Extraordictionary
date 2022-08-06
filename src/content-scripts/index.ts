fd26bd64e868eb0c4a033c4181100d1d89da36e7
thamph <poomz180@gmail.com>
2022-08-06 11:49:27 +0700

    display popup near selected text

--- src/content-scripts/index.ts
+++ src/content-scripts/index.ts
@@ -1,14 +1,14 @@
 let popup: HTMLIFrameElement = document.createElement("iframe");
-let selectedText: Selection | string | null = "";
+let selectedText: Selection | null;
 
 const initPopup = () => {
   popup.src = chrome.runtime.getURL("popup/index.html");
   popup.style.width = "580px";
   popup.style.height = "375px";
-  popup.style.position = "fixed";
+  popup.style.position = "absolute";
   popup.style.right = "0";
   popup.style.top = "0";
-  popup.style.zIndex = "1000000000";
+  popup.style.zIndex = "2000";
   popup.style.transform = "scale(0.85)";
   popup.style.transformOrigin = "top right";
   popup.style.backgroundColor = "white";
@@ -20,12 +20,24 @@ const openPopup = () => {
   setTimeout(() => {
     selectedText = window.getSelection();
     if (selectedText) {
-      selectedText = selectedText.toString();
-      if (selectedText.length > 0 && selectedText.length < 100) {
+      if (selectedText.toString().length > 0 && selectedText.toString().length < 100) {
         // reload iframe
         popup.src = popup.src;
 
         popup.style.visibility = "visible";
+
+        //display popup near selected text
+        const selectedTextPosition: DOMRect = selectedText.getRangeAt(0).getBoundingClientRect();
+        if (selectedTextPosition.left + parseInt(popup.style.width) > window.innerWidth) {
+          popup.style.left = (selectedTextPosition.left - parseInt(popup.style.width)).toString() + "px";
+        } else {
+          popup.style.left = selectedTextPosition.left.toString() + "px";
+        }
+        if (selectedTextPosition.top + parseInt(popup.style.height) - 30 > window.innerHeight) {
+          popup.style.top = (selectedTextPosition.top - parseInt(popup.style.height) + window.scrollY).toString() + "px";
+        } else {
+          popup.style.top = (selectedTextPosition.top + window.scrollY).toString() + "px";
+        }
       }
     }
   }, 500);
@@ -42,7 +54,11 @@ window.addEventListener("mousedown", () => {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "getSelectedText") {
-    sendResponse(selectedText);
+    if (selectedText) {
+      sendResponse(selectedText.toString());
+    } else {
+      sendResponse("");
+    }
   }
 });
 
